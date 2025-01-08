c797111bd850f2e7338bf5eef3f6c39a453a4909
higuchi-learn <haruki.higuchi.000@gmail.com>
2025-01-08 20:07:03 +0900

    タスクの編集、削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    p task_params
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render "new", status: :unprocessable_entity
+      #render -> viewsフォルダ内のnew.html.erbを表示
+      #status: バリデーションエラーが起こった場合、ステータスコード422でレンダリング
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
+    #HTTP stutas code 303 を使用（新規リソースへの転送）
   end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5}
+  #validates:title -> titleカラムについてチェック
+  #presence: ture 未入力チェック
 end
